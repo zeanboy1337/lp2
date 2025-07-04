ba657cc99782ac3e05f01d7130894ff68db4e3ab
zeanboy1337 <zeanboy1337@gmail.com>
2025-07-05 04:55:06 +0700

    Update and rename sitemap.xml to upload_sftp.php

--- upload_sftp.php
+++ upload_sftp.php
@@ -2,7 +2,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Lokasi phpseclib manual
+// Load library
+require_once __DIR__ . '/phpseclib-master/phpseclib/Net/SSH2.php';
 require_once __DIR__ . '/phpseclib-master/phpseclib/Net/SFTP.php';
 
 use phpseclib\Net\SFTP;
@@ -12,15 +13,16 @@ $username = 'adminptyogov';
 $password = 'psacln';
 
 $sftp = new SFTP($host);
+
 if (!$sftp->login($username, $password)) {
-    die('Login failed');
+    die('❌ Login failed');
 }
 
 $localFile = __DIR__ . '/localfile.txt';
 $remoteFile = '/home/adminptyogov/remote_uploaded_file.txt';
 
 if (!$sftp->put($remoteFile, file_get_contents($localFile))) {
-    die('Upload failed');
+    die('❌ Upload failed');
 }
 
 echo "✅ Upload berhasil ke SFTP!";
