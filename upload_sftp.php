c5e523ea77addbf8d0a97807334071c5b8bef20b
zeanboy1337 <zeanboy1337@gmail.com>
2025-07-05 05:12:55 +0700

    Update upload_sftp.php

--- upload_sftp.php
+++ upload_sftp.php
@@ -2,16 +2,10 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-// Include kelas fungsi dasar yang dipakai SFTP/SSH2
+// Hanya require file yang ada saja
 require_once __DIR__ . '/phpseclib-master/phpseclib3/Common/Functions/Strings.php';
-require_once __DIR__ . '/phpseclib-master/phpseclib3/Common/Functions/Multibyte.php';
-require_once __DIR__ . '/phpseclib-master/phpseclib3/Common/Functions/JSON.php';
-require_once __DIR__ . '/phpseclib-master/phpseclib3/Common/Functions/UTF8.php';
-
-// Trait constant utility dipakai kelas SFTP/SSH2
 require_once __DIR__ . '/phpseclib-master/phpseclib3/Common/ConstantUtilityTrait.php';
 
-// Kelas utama SSH2 dan SFTP
 require_once __DIR__ . '/phpseclib-master/phpseclib3/Net/SSH2.php';
 require_once __DIR__ . '/phpseclib-master/phpseclib3/Net/SFTP.php';
 
